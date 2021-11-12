a76d080f4c1547f84cb74eea74fa8c792db9edbc
Lucas Silva <martssilvapro@gmail.com>
2021-11-12 16:01:21 -0300

    cria service auth e configura modulo auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,20 @@
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './../app/users/users.module';
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      privateKey: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService],
+})
 export class AuthModule {}
