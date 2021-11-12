6246896cdaa4ccb438b67585d3e894e0d372acec
Lucas Silva <martssilvapro@gmail.com>
2021-11-12 16:11:17 -0300

    adiciona arquivo de estrategia da regra de negocio

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('api/auth')
+export class AuthController {
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login() {}
+}
