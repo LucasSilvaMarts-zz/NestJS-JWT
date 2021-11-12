5112cb02cec1eae01183e003e4e6b338642a5c5c
Lucas Silva <martssilvapro@gmail.com>
2021-11-12 16:10:45 -0300

    configura controller de login com autenticacao

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
@@ -16,5 +17,6 @@ import { PassportModule } from '@nestjs/passport';
     }),
   ],
   providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
